0252d76844ac8d85c72f82f00c9583c0c184912a
imcnb <chaminduherath329@gmail.com>
2025-08-26 21:38:47 +0530

    jpg

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,10 +7,10 @@ import { ProductContext } from '../context/ProductContext';
 import styles from './HomePage.module.css';
 
 const heroImages = [
-  "/images/hero/hero1.jpeg",
-  "/images/hero/hero2.jpeg",
-  "/images/hero/hero3.jpeg",
-  "/images/hero/hero4.jpeg",
+  "/images/hero/hero1.JPG",
+  "/images/hero/hero2.JPG",
+  "/images/hero/hero3.JPG",
+  "/images/hero/hero4.JPG",
 ];
 
 const SLIDE_INTERVAL = 4000;
