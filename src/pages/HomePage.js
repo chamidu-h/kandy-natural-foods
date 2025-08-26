e32fd64efce0e8d8d779275f92c59b10f702aff7
imcnb <chaminduherath329@gmail.com>
2025-08-26 23:23:56 +0530

    final

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import Button from '../components/common/Button';
 import ProductCard from '../components/products/ProductCard';
 import { ProductContext } from '../context/ProductContext';
-import ApiTest from '../components/ApiTest';
 import styles from './HomePage.module.css';
 
 const heroImages = [
@@ -40,7 +39,6 @@ const HomePage = () => {
         <title>Your Sweet Shop | Handcrafted Sri Lankan Sweets</title>
         <meta name="description" content="Experience the authentic taste of Sri Lanka. We create traditional, handcrafted sweets with love, using the finest local ingredients." />
       </Helmet>
-      <ApiTest />
       
       {/* Cinematic Hero Section */}
       <section className={styles.hero}>
